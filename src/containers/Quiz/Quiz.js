83e7fcc7f0e4b30672b17b4a22616ef9707a6553
Pavel dub <dubstr1@gmail.com>
2020-05-17 12:56:44 +0300

    active Item handler

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react'
-import classes from './Quiz.module.css'
+import React, { Component } from 'react';
+import classes from './Quiz.module.css';
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 class Quiz extends Component {
-  state = {
-    quiz: []
-  }
+	state = {
+		quiz: [
+			{
+				question: 'Какого цвета небо?',
+				rightAnswerId: 2,
+				answers: [
+					{
+						text: 'Черный',
+						id: 1
+					},
+					{
+						text: 'Голубой',
+						id: 2
+					},
+					{
+						text: 'Красный',
+						id: 3
+					},
+					{
+						text: 'Зеленый',
+						id: 4
+					}
+				]
+			}
+		]
+	};
 
-  render () {
-    return (
-      <div className={classes.Quiz}>
-        <h1>Quiz</h1>
-      </div>
-    )
-  }
+	answerOnClickHandler = (answerId) => {
+		console.log('answer Id', answerId);
+	};
+
+	render() {
+		return (
+			<div className={classes.Quiz}>
+				<div className={classes.QuizWrapper}>
+					<h1>Ответьте на все вопросы:</h1>
+					<ActiveQuiz
+						answers={this.state.quiz[0].answers}
+						question={this.state.quiz[0].question}
+						onAnswerClick={this.answerOnClickHandler}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Quiz
+export default Quiz;
